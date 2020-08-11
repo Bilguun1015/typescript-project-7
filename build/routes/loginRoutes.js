0c080fb1579b7e6aa9946524e4582a0983d64351
Bilguun <bbg.1015@gmail.com>
2020-08-10 20:43:11 -0500

    added protected route and logout route

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -2,6 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
+//middleware
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    // forbidden
+    res.status(403);
+    res.send('Not permitted');
+}
 var router = express_1.Router();
 exports.router = router;
 router.get('/login', function (req, res) {
@@ -27,4 +37,11 @@ router.get('/', function (req, res) {
     else {
         res.send("\n      <div>\n        <div>\n          You are not logged in!\n          <a href=\"/login\">Login</a>\n        </div>\n      </div>\n    ");
     }
+});
+router.get('/logout', function (req, res) {
+    req.session = null;
+    res.redirect('/');
+});
+router.get('/protected', requireAuth, function (req, res) {
+    res.send('Welcome to protected route, logged in user');
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,7 +1,18 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
+}
+
+//middleware
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+  // forbidden
+  res.status(403);
+  res.send('Not permitted');
 }
 
 const router = Router();
@@ -56,6 +67,15 @@ router.get('/', (req, res) => {
       </div>
     `);
   }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = null;
+  res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected route, logged in user');
 });
 
 export { router };
